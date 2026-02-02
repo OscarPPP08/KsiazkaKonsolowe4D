14864954361e346512aa45bee9fd00604629c536
Mirosław <mirek@gmail.com>
2026-02-02 12:02:57 +0100

    Implementacja klasy "Ksiazka"

---
+++ KsiazkaKonsolowe4D/Ksiazka.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsiazkaKonsolowe4D
+{
+    internal class Ksiazka
+    {
+        public List<Strona> strony = new List<Strona>();
+        
+        public Ksiazka(string tytul)
+        {
+            Strona strona = new Strona(tytul, 1);
+            strony.Add(strona);
+        }
+
+        public void DodajStrone(string trescStrony)
+        {
+            var Strona = new Strona(trescStrony);
+        }
+    }
+}
