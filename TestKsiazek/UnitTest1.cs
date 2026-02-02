738e522c51d2f1bf230b76cd0470d666897a76bb
Mirosław <mirek@gmail.com>
2026-02-02 13:36:15 +0100

    Usunieci redundancji w testach jednostkowych

--- TestKsiazek/UnitTest1.cs
+++ TestKsiazek/UnitTest1.cs
@@ -5,6 +5,19 @@ namespace TestKsiazek
     [TestClass]
     public class UnitTest1
     {
+        public Ksiazka metodaTworzacaKsiazke()
+        {
+            string daneZPliku = File.ReadAllText("C:\\Users\\Student\\Desktop\\dane.txt");
+
+            string[] tablicaStron = daneZPliku.Split(';');
+
+            Ksiazka ksiazka = new Ksiazka(tablicaStron[0]);
+            for (int i = 1; i < tablicaStron.Length; i++)
+            {
+                ksiazka.DodajStrone(tablicaStron[i]);
+            }
+            return ksiazka;
+        }
         [TestMethod]
         public void TestowaniePierwszejStrony()
         {
@@ -20,15 +33,7 @@ namespace TestKsiazek
         public void TestowanieWczytywaniaZPliku()
         {
             //Arange
-            string daneZPliku = File.ReadAllText("C:\\Users\\Student\\Desktop\\dane.txt");
-
-            string[] tablicaStron = daneZPliku.Split(';');
-
-            Ksiazka ksiazka = new Ksiazka(tablicaStron[0]);
-            for (int i = 1; i < tablicaStron.Length; i++)
-            {
-                ksiazka.DodajStrone(tablicaStron[i]);
-            }
+            Ksiazka ksiazka = metodaTworzacaKsiazke();
             //Act
             string daneDoTestowania = "Powrót do miasta nie by³ ju¿ ucieczk¹, lecz wyborem. Ulice wydawa³y siê jaœniejsze, a ludzie mniej obcy. Adam zatrzyma³ siê przy tym samym zegarze, który kiedyœ go niepokoi³. Tym razem uœmiechn¹³ siê, widz¹c jego niedok³adnoœæ. Czas nie by³ wrogiem, lecz towarzyszem. Wiedzia³, ¿e przed nim jeszcze wiele niewiadomych, ale nie ba³ siê ich jak dawniej. Z listem schowanym g³êboko w kieszeni ruszy³ przed siebie, gotów pisaæ dalsz¹ czêœæ w³asnej historii.";
             //Assert
@@ -38,15 +43,10 @@ namespace TestKsiazek
         public void TestowanieIlosciStron()
         {
             //Arange
+            Ksiazka ksiazka = metodaTworzacaKsiazke();
             string daneZPliku = File.ReadAllText("C:\\Users\\Student\\Desktop\\dane.txt");
 
             string[] tablicaStron = daneZPliku.Split(';');
-
-            Ksiazka ksiazka = new Ksiazka(tablicaStron[0]);
-            for (int i = 1; i < tablicaStron.Length; i++)
-            {
-                ksiazka.DodajStrone(tablicaStron[i]);
-            }
             //Act
             //Assert
             Assert.AreEqual(ksiazka.strony.Count, tablicaStron.Length);
