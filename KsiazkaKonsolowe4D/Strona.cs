052d45ceea1a2875c379792ef6540e980b26fb01
Mirosław <mirek@gmail.com>
2026-02-02 11:55:42 +0100

    Implementacja klasy "Strona"

---
+++ KsiazkaKonsolowe4D/Strona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsiazkaKonsolowe4D
+{
+    internal class Strona
+    {
+        public string trescStrony;
+        public uint numerStrony;
+        public static uint licznikStrony = 0;
+
+        public Strona(string trescStrony, uint numerStrony)
+        {
+            this.trescStrony = trescStrony;
+            this.numerStrony = numerStrony;
+            licznikStrony = numerStrony + 1;
+        }
+
+        public Strona(string trescStrony)
+        {
+            this.trescStrony = trescStrony;
+            this.numerStrony = licznikStrony++;
+        }
+    }
+}
